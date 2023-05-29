7b8af4d0acab152d7ea0f74cd18f232b24a6cbd1
itsoxid <peterbilewu1@gmail.com>
2023-05-29 07:22:33 +0100

    potentially fixed jumpy master villager

--- src/main/java/net/oxid/tutorialmod/villager/ModVillagers.java
+++ src/main/java/net/oxid/tutorialmod/villager/ModVillagers.java
@@ -4,6 +4,9 @@ import com.google.common.collect.ImmutableSet;
 import net.fabricmc.fabric.api.object.builder.v1.trade.TradeOfferHelper;
 import net.fabricmc.fabric.api.object.builder.v1.villager.VillagerProfessionBuilder;
 import net.fabricmc.fabric.api.object.builder.v1.world.poi.PointOfInterestHelper;
+import net.oxid.tutorialmod.TutorialMod;
+import net.oxid.tutorialmod.block.ModBlocks;
+import net.oxid.tutorialmod.item.ModItems;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
@@ -14,8 +17,7 @@ import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.village.TradeOffer;
 import net.minecraft.village.VillagerProfession;
 import net.minecraft.world.poi.PointOfInterestType;
-import net.oxid.tutorialmod.TutorialMod;
-import net.oxid.tutorialmod.block.ModBlocks;
+
 
 public class ModVillagers {
     public static final PointOfInterestType JUMPY_POI = registerPOI("jumpy_poi", ModBlocks.JUMPY_BLOCK);
@@ -33,18 +35,18 @@ public class ModVillagers {
                 1, 1, ImmutableSet.copyOf(block.getStateManager().getStates()));
     }
 
-     public static void registerVillagers() {
-         TutorialMod.LOGGER.debug("Registering Villagers for " + TutorialMod.MOD_ID);
-     }
+    public static void registerVillagers() {
+        TutorialMod.LOGGER.debug("Registering Villagers for " + TutorialMod.MOD_ID);
+    }
 
-     public static void registerTrades() {
-         TradeOfferHelper.registerVillagerOffers(JUMP_MASTER, 1,
-                 factories -> {
+    public static void registerTrades() {
+        TradeOfferHelper.registerVillagerOffers(JUMP_MASTER,1,
+                factories -> {
                     factories.add(((entity, random) -> new TradeOffer(
                             new ItemStack(Items.EMERALD, 3),
-                            new ItemStack(ModBlocks.JUMPY_BLOCK, 1),
-                            6,2,0.02f
+                            new ItemStack(ModItems.EIGHT_BALL, 1),
+                            6, 2, 0.02f
                     )));
-                 });
-     }
+                });
+    }
 }
